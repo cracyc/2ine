53682aa0b9b312f2634fbb3fd860baa4591bcd40
cracyc <cracyc@users.noreply.github.com>
2018-07-11 19:27:40 -0500

    add stub os2sm and pmgre

---
+++ native/pmgre.c
@@ -0,0 +1,16 @@
+/**
+ * 2ine; an OS/2 emulator for Linux.
+ *
+ * Please see the file LICENSE.txt in the source's root directory.
+ *
+ *  This file written by Ryan C. Gordon.
+ */
+
+#include "os2native.h"
+#include "pmgre.h"
+
+LX_NATIVE_MODULE_INIT()
+LX_NATIVE_MODULE_INIT_END()
+
+// end of pmgre.c ...
+
